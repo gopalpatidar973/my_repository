9f86c55a8cf53474eed0d636af719f3c23d8271a
Gopal Patidar <Gopal.Patidar@infobellit.com>
2025-04-09 16:41:42 +0530

    first v1 of prime.py

---
+++ prime.py
@@ -0,0 +1,30 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def print_n_primes(n):
+    count = 0
+    num = 2
+    while count < n:
+        if is_prime(num):
+            print(num, end=' ')
+            count += 1
+        num += 1
+
+
+try:
+    n=int(input("Enter the number of prime numbers to print: "))
+    if n>0:
+        print_n_primes(n)
+    else:
+        print("Please enter a positive integer.")
+except ValueError:
+    print("Invalid input. Please enter an integer.")
